e8614d4746751936d3b2ecb28d1b20bb8158c654
Oleh Bodunov <oleg.bdnf@gmail.com>
2019-06-02 16:35:08 +0200

    Update master_setup.sh

--- master_setup.sh
+++ master_setup.sh
@@ -9,3 +9,11 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo kubeadm init
+
+# make kubeadm run as non root user
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+kubectl get pods --all-namespaces
