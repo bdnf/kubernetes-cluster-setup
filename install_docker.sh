b9ffe295cbe23d51ba5ab8fadd45f4674ae534a6
root <oleg.bdnf@gmail.com>
2019-06-02 10:28:38 -0400

    minor improvements

--- install_docker.sh
+++ install_docker.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # install prerequisites
-apt-get update
-apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get update
+suodapt-get -y install apt-transport-https ca-certificates curl software-properties-common
 
 # install docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # Add repository
-add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 # Install latest stable docker stable version
-apt-get update
-apt-get -y install docker-ce
+sudo apt-get update
+sudo apt-get -y install docker-ce
 
 # install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod a+x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod a+x /usr/local/bin/docker-compose
 # Enable & start docker
-systemctl enable docker
-systemctl start docker
+sudo systemctl enable docker
+sudo systemctl start docker
