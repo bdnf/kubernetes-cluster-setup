5bdcb02c58b5056a9dccab36328a58251b260059
Oleh Bodunov <oleg.bdnf@gmail.com>
2019-06-25 20:55:13 +0200

    Update master_teardown.sh

--- master_teardown.sh
+++ master_teardown.sh
@@ -3,4 +3,5 @@ export MASTER=
 kubectl drain $MASTER --delete-local-data --force --ignore-daemonsets
 kubectl delete node $MASTER
 kubeadm reset
-iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
+
+
