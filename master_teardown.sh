edca703e23b230d64d597e5fae7fe7ef910e7b7d
Oleh Bodunov <oleg.bdnf@gmail.com>
2019-06-25 20:59:02 +0200

    Update master_teardown.sh

--- master_teardown.sh
+++ master_teardown.sh
@@ -4,4 +4,6 @@ kubectl drain $MASTER --delete-local-data --force --ignore-daemonsets
 kubectl delete node $MASTER
 kubeadm reset
 
-
+# in testing environment
+# sudo rm -rf ~/.kube
+# sudo rm /var/lib/etcd
