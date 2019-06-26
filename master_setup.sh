2bb6d1800ea288c09c18807fee657480c1e0391c
Oleh Bodunov <oleg.bdnf@gmail.com>
2019-06-26 14:42:12 +0200

    Update master_setup.sh

--- master_setup.sh
+++ master_setup.sh
@@ -9,7 +9,10 @@ EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
-kubeadm init --pod-network-cidr=10.244.0.0/16 # add --apiserver-advertise-address="ip" if you want to use a different IP address than the main server IP
+#kubeadm init --pod-network-cidr=10.244.0.0/16 # --apiserver-advertise-address="ip" if you want to use a different IP address than the main server IP
+
+#for hosted solution
+kubeadm init --pod-network-cidr=192.168.0.0/16 
 
 echo "cluster initialized"
 sleep 2
@@ -20,9 +23,10 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 sleep 2
 echo "deploying pod netword"
 
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-# kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-# kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+#for hosted solution
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 
 echo "cluster initialized"
 kubectl get pods --all-namespaces
