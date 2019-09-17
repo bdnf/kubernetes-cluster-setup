263f5c97177df1983b8a6fcc57884af4767d510d
Oleh Bodunov <oleg.bdnf@gmail.com>
2019-09-17 11:38:05 +0200

    Update weave ports

--- master_init_weave.sh
+++ master_init_weave.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo kubeadm init --kubernetes-version v1.15.0 --apiserver-advertise-address="your ip"
+sudo kubeadm init --kubernetes-version v1.15.3 --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address="your ip"
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo cp /etc/kubernetes/admin.conf $HOME/
+
 sudo chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
 
@@ -16,9 +19,15 @@ sudo ufw allow 6763/udp
 sudo ufw allow 6764/udp
 sudo ufw allow 6781/tcp
 sudo ufw allow 6782/tcp
+sudo ufw allow 6783/tcp
+sudo ufw allow 6783/udp
+sudo ufw allow 6784/tcp
+sudo ufw allow 6784/udp
 
 #Kubeadm master 
 sudo ufw allow 6443,2379,2380,10250,10251,10252,10255/tcp
 
 #Nodes
-# sudo ufw allow 10250,10255,30000:32767/tcp
+sudo ufw allow 10250,10255,30000:32767/tcp
+sudo ufw allow 6784/tcp
+sudo ufw allow 6784/udp
