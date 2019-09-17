8fb42245c1c325aff020f1a41b0c3442a7a2ebf6
Oleh Bodunov <oleg.bdnf@gmail.com>
2019-09-17 11:42:02 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,15 +2,22 @@
 
 # install prerequisites
 sudo apt-get update
-suodapt-get -y install apt-transport-https ca-certificates curl software-properties-common
-
-# install docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# Add repository
-sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# Install latest stable docker stable version
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt-get update
-sudo apt-get -y install docker-ce
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
 
 # install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
