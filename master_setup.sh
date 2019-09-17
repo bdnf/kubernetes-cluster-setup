ef0de46ae74677a8c7b6c1aa3fd9768af41a7c47
Oleh Bodunov <oleg.bdnf@gmail.com>
2019-09-17 11:38:52 +0200

    Update master_setup.sh

--- master_setup.sh
+++ master_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "installing kubernetes"
-apt-get update && apt-get install -y apt-transport-https curl
+sudo bash -c 'apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
- deb https://apt.kubernetes.io/ kubernetes-xenial main
+deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+apt-get update'
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
